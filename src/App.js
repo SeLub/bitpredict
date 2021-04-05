7ac559e290c13fc62a941e48d6b60f1cb3b59da1
SeLub <siteup24@gmail.com>
2021-04-06 01:41:57 +0300

    Business Logic func implemented for Line 2 in Graph

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import {getAPIdata, dataObj} from './API';
 import Template from './Template'
 import {GraphDraw} from './Graph'
+import SomeBusinessLogic from './BusinessLogic'
 import './App.css';
 
 
@@ -10,14 +11,14 @@ function App() {
   const [dataObject, setDataObj] = useState(null);
   let [panelHeader, panelFooter, panelData, panelSidebar] = ['Bitcoin Price Analysis and Extrapolation predict', 'Data Updated ', 'Test Data', 'Test SideBar']
   panelFooter += dataObj.dataTime;
-
+console.log(dataObj)
 
   useEffect(() => {
     getAPIdata()
         .then(data => {
           let line1 = dataObj.dataArr ;
-          let line2 = dataObj.dataArr);
-          setDataObj(data.bpi);
+          let line2 = SomeBusinessLogic(dataObj.dataArr)
+          setDataObj(data);
           GraphDraw(dataObj.titles,line1, line2);
         });
     }, []);
