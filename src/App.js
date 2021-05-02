56109a7190a9dfb5eac6cbc4d9dfc612a0c335ce
SeLub <siteup24@gmail.com>
2021-05-03 01:55:50 +0300

    remove old code ./API ./Services from App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Template from './Template'
-import dataObj from './API';
-import {GraphDraw} from './Graph'
-import { getData } from './Services';
-import SomeBusinessLogic from './BusinessLogic'
+import useFetch from './API';
+//import {GraphDraw} from './Graph'
+//import SomeBusinessLogic from './BusinessLogic'
 
 
 function App() {
-  const [dataObject, setDataObj] = useState(null);
   let [panelHeader, panelFooter, panelData, panelSidebar] = ['Bitcoin Price Analysis and Extrapolation predict', 'Data Updated ', 'Test Data', 'Test SideBar']
-      panelFooter += dataObj.dataTime;
-      
-      useEffect(() => {
-  getData()
-      .then(data => {
-        let line1 = dataObj.dataArr ;
-        let line2 = SomeBusinessLogic(dataObj.dataArr)
-        setDataObj(data.bpi);
-        GraphDraw(dataObj.titles,line1, line2)
-      });
-  }, []);
 
 return (
     <div className="App">
