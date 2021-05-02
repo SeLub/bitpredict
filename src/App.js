7f29588e24d2f65dbe72f6903185b61fbae5d453
SeLub <siteup24@gmail.com>
2021-05-03 02:05:58 +0300

    App with custom hook fetch ready

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ import useFetch from './API';
 
 function App() {
   let [panelHeader, panelFooter, panelData, panelSidebar] = ['Bitcoin Price Analysis and Extrapolation predict', 'Data Updated ', 'Test Data', 'Test SideBar']
+  const { loading, data, error } = useFetch('https://api.coindesk.com/v1/bpi/historical/close.json')
+  if (loading) return 'LOADING...'
+  if (error) { console.log(error); return null }
+
 
 return (
     <div className="App">
       <header className="App-header">
+      {JSON.stringify(data)}
           <Template 
           panelHeader={panelHeader}
           panelFooter={panelFooter}
