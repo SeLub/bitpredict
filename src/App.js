030303d5449195ff413e9abafc9e39dd75d7e3ae
SeLub <siteup24@gmail.com>
2021-06-17 00:02:50 +0300

    change SETTINS data to fetch from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './App.css';
 import Template from './Template'
 import Footer from './Components/Footer'
@@ -11,10 +11,11 @@ import { LanguageContext } from './context'
 import * as SETTINGS from './SETTINGS'
 
 function App() {
+  //озможно оставть данные по умолчанию, чтобы их использовать для работы без интернета оф-лайн
   //* Load data for Graph from './SETTINGS' anf fix the state
-  let [titleChart, lablesChart, dataChart] = [SETTINGS.TITLE,SETTINGS.LABELS,SETTINGS.DATA]
+  // let [titleChart, lablesChart, dataChart] = [SETTINGS.TITLE,SETTINGS.LABELS,SETTINGS.DATA]
   //const [chart, setChart] = useState({titleChart, lablesChart, dataChart})
-const [chart] = useState({titleChart, lablesChart, dataChart})
+  const [chart, setChart] = useState({})
   //* Define props for Template with inintial settings
   let [panelHeader, panelFooter, panelMain, panelSecondLine] = ['Bitcoin Price Analysis and Extrapolation predict', 'Data Updated ', 'Main Panel', 'panelSecondLine']
   
@@ -28,8 +29,32 @@ const [chart] = useState({titleChart, lablesChart, dataChart})
   //Set initial state for 'language'
   const [langContext, setLangContext] = useState(localStorage.getItem('language') || 'ENG')
 
+  useEffect(()=>{
+  
+  const getActualData = (obj) =>{   
+    let dataObj = {
+        titleChart:`Bicoin Prices, USD\n`,
+        lablesChart:[],
+        dataChart:[]
+        }
 
-   
+    for (let [key, value] of Object.entries(obj.bpi)) {
+        dataObj.lablesChart.push(key)
+        dataObj.dataChart.push(value)
+    }
+    return dataObj
+  }
+
+    const fetchAPIdata = async() => {
+      const response = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
+      const APIdata = await response.json()
+      const API = await getActualData(APIdata) 
+      setChart(API)
+    }
+    fetchAPIdata()
+  }
+    ,[])
+
   //* Set props for Template. Props are our Components
   panelHeader = <Header showSettingsPanel={showSettingsPanel}/>
   panelMain = <MainPanel dash={dash} toggleTheme={toggleTheme} chart={chart} setDash={setDash}/>
--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -3,33 +3,23 @@ import { Line } from 'react-chartjs-2'
 
   const LineDemo = ({chart}) =>{
 
-  const LABELS = ['2021-05-14','2021-05-15','2021-05-16','2021-05-17','2021-05-18','2021-05-19','2021-05-20','2021-05-21','2021-05-22','2021-05-23','2021-05-24','2021-05-25','2021-05-26','2021-05-27']
-  const TITLE = `Bicoin Prices, USD\n`
-  const DATA = [50000, 51000, 52000, 49500, 48000, 47000, 48000,50000, 51000, 52000, 49500, 48000, 47000, 46000]
+  const colorLine = (ctx, valueUp, valueDown) => { if (ctx.p0.parsed.y < ctx.p1.parsed.y) {return valueUp} else {return valueDown}}
 
-const colorLine = (ctx, valueUp, valueDown) => 
-{
-  if (ctx.p0.parsed.y < ctx.p1.parsed.y) { console.log(ctx.p1.parsed); return valueUp} else {return valueDown}
-}
+  const dataChart = {
+            labels: chart.lablesChart, 
+            datasets: 
+              [{
+              label: chart.titleChart,
+              data: chart.dataChart,
+              fill: false,
+              borderColor: 'red',
+              backgroundColor: 'white',
+              tension: 0.1,
+              segment: { borderColor: ctx => colorLine(ctx, 'green', 'red')}
+              }]
+  }
 
-      const dataChart = {
-          labels: LABELS, 
-            datasets: [{
-            label: TITLE,
-            data: DATA,
-            fill: false,
-            borderColor: 'red',
-            backgroundColor: 'white',
-            tension: 0.1,
-            segment: {
-        borderColor: ctx => colorLine(ctx, 'green', 'red')
-
-      }
-            }]
-      };
-
-      const optionsChart = {
-
+  const optionsChart = {
     scales: {
       yAxes: [
         {
@@ -41,7 +31,7 @@ const colorLine = (ctx, valueUp, valueDown) =>
     },
   }
     return(
-      <>
+     <>
      <Line data={dataChart} options={optionsChart} />
      </>
     )}
